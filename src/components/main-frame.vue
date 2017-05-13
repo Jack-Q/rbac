c14f307e583703191ccab22bc816959747b9abc8
Jack Q <QiaoBo@outlook.com>
2017-05-13 19:54:28 +0800

    dist config tewak

--- src/components/main-frame.vue
+++ src/components/main-frame.vue
@@ -15,7 +15,7 @@
         <span>Access</span>
         <span>Control</span>
       </div>
-      <div class="header-right">
+      <a href="https://github.com/Jack-Q/" class="header-right">
         <svg height="20" aria-label="code" viewBox="0 0 14 16" version="1.1" width="17" role="img">
           <path d="M9.5 3L8 4.5 11.5 8 8 11.5 9.5 13 14 8 9.5 3zm-5 0L0 8l4.5 5L6 11.5 2.5 8 6 4.5 4.5 3z"></path>
         </svg>
@@ -24,8 +24,8 @@
           <path d="M11.2 3c-.52-.63-1.25-.95-2.2-1-.97 0-1.69.42-2.2 1-.51.58-.78.92-.8 1-.02-.08-.28-.42-.8-1-.52-.58-1.17-1-2.2-1-.95.05-1.69.38-2.2 1-.52.61-.78 1.28-.8 2 0 .52.09 1.52.67 2.67C1.25 8.82 3.01 10.61 6 13c2.98-2.39 4.77-4.17 5.34-5.33C11.91 6.51 12 5.5 12 5c-.02-.72-.28-1.39-.8-2.02V3z"></path>
         </svg>
         by
-        <a href="https://github.com/Jack-Q/">Jack Q</a>
-      </div>
+        Jack Q
+      </a>
     </div>
     <div class="main">
       <div class="main-left">
@@ -171,9 +171,12 @@ a {
 }
 
 .header-right {
+  display: block;
   padding-right: 70px;
   color: #999;
   transition: all ease 400ms;
+  color: inherit;
+  text-decoration: none;
 }
 
 .header-right:hover {
@@ -183,10 +186,6 @@ a {
   background: rgba(255, 255, 255, 0.25);
 }
 
-.header-right a {
-  color: inherit;
-  text-decoration: none;
-}
 
 .header-right svg {
   fill: #999;
